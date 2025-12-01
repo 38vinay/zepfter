cf442d32e21b5a33879d94a6fe79ed19217ed752
JaswanthRajigiri <116971195+38vinay@users.noreply.github.com>
2025-12-01 22:25:18 +0530

    changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { Link, useLocation } from "react-router-dom";
-import logo from "../assets/logo2.png";
+import logo from "../assets/logo2-removebg-preview.png";
 
 const Header = () => {
   const [scrolled, setScrolled] = useState(false);
